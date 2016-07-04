753cb3d3aa2723c52d2d08dd12429d4ffdf23b97
Rafael Nery <rafanery0@gmail.com>
2016-07-03 22:41:00 -0300

    Reconfigurando

--- test/fixtures/config.ubuntu.sh
+++ test/fixtures/config.ubuntu.sh
@@ -1,8 +1,6 @@
 sudo mkdir /var/cvs
 sudo cvs -d /var/cvs init
-
 sudo cp test/fixtures/cvspserver /etc/xinetd.d/cvspserver
-
-/etc/init.d/xinetd restart
+sudo service xinetd restart
 
 netstat -tap | grep cvs

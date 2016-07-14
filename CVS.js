203dfbfb1ae18315e5d2bae3c5674f489c0ef044
Maurício Costa <mauriciocosta.cm@gmail.com>
2016-07-13 22:30:48 -0300

    Adicionado estrutura inicial.

---
+++ CVS.js
@@ -0,0 +1,30 @@
+"use strict"
+
+var Init = require("./src/Command/init.js"),
+    Login = require("./src/Command/login.js");
+
+class Repository {
+
+  construct(root) {
+    this._root = root;
+  }
+
+  init() {
+
+  }
+
+  login() {
+
+  }
+
+  get root() {
+    return this._root;
+  }
+}
+
+module.exports = Repository;
+
+
+"use strict";
+
+module.exports = {};

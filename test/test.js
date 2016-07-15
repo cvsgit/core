5f4e0af7dcfd23a60c0a34990137394147173ae4
Maurício Costa <mauriciocosta.cm@gmail.com>
2016-07-15 00:25:51 -0300

    Adicionando teste.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,28 @@
 var expect = require("chai").expect;
+var fs = require("fs");
+
+var cvs = require("../CVS");
 
 describe("CVS", function() {
 
-  it("Test", function() {
+  describe("Init", function() {
 
+    it ("Local", function() {
+
+      var localrep = "/tmp/reptest";
+
+      fs.mkdirSync(localrep)
+      expect(fs.readdirSync(localrep)).to.have.length(0)
+
+      var repo = new cvs(localrep);
+      repo.init().catch(function(err) {
+        throw err;
+      })
+
+      var dir = fs.readdirSync(localrep)
+      expect(dir).to.have.length(1);
+      expect(dir[0]).to.equal("CVSROOT");
+    })
   })
+
 });
